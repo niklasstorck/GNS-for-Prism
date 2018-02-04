4ea76b17c519e545025ee0e745aeade38600bc84
Niklas Storck <niklas@family-storck.se>
2018-02-04 14:52:44 +0100

    First publish on Github

---
+++ Main.dfm
@@ -0,0 +1,86 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'Form1'
+  ClientHeight = 479
+  ClientWidth = 873
+  Color = clBackground
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object W7NavigationButton1: TW7NavigationButton
+    Left = 383
+    Top = 29
+    Width = 24
+    Height = 24
+    Caption = 'W7NavigationButton1'
+    FadeInInterval = 17
+    FadeOutInterval = 17
+    ParentFont = False
+    OnClick = W7NavigationButton1Click
+  end
+  object LabelInfo: TLabel
+    Left = 440
+    Top = 25
+    Width = 36
+    Height = 25
+    Caption = 'Idle'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -21
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object EditFolder: TLabeledEdit
+    Left = 24
+    Top = 32
+    Width = 353
+    Height = 21
+    Color = clScrollBar
+    EditLabel.Width = 133
+    EditLabel.Height = 13
+    EditLabel.Caption = 'Map for Prism log_obs_auto'
+    TabOrder = 0
+  end
+  object Memo1: TMemo
+    Left = 24
+    Top = 80
+    Width = 793
+    Height = 329
+    Color = clScrollBar
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 1
+  end
+  object Button1: TButton
+    Left = 728
+    Top = 432
+    Width = 75
+    Height = 25
+    Caption = 'Run'
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 72
+    Top = 312
+  end
+  object TimerSlow: TTimer
+    OnTimer = TimerSlowTimer
+    Left = 200
+    Top = 312
+  end
+  object TimerFast: TTimer
+    OnTimer = TimerFastTimer
+    Left = 488
+    Top = 296
+  end
+end
---
+++ Main.pas
@@ -0,0 +1,164 @@
+unit Main;
+
+interface
+
+uses
+  ShellApi,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FolderDialog, Vcl.StdCtrls, Vcl.ExtCtrls,
+  W7Classes, W7NaviButtons;
+
+type
+  TForm1 = class(TForm)
+    OpenDialog1: TOpenDialog;
+    EditFolder: TLabeledEdit;
+    W7NavigationButton1: TW7NavigationButton;
+    Memo1: TMemo;
+    Button1: TButton;
+    TimerSlow: TTimer;
+    LabelInfo: TLabel;
+    TimerFast: TTimer;
+    procedure W7NavigationButton1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure TimerSlowTimer(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure TimerFastTimer(Sender: TObject);
+  private
+    SLast: String;
+    procedure CallGNS(var Return: integer; SNew: string);
+    function ObsEnd(SNew: String):Boolean;
+    function StripTimeFromString(var STemp: string):String;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  TimerSlow.Interval:=10000;
+  TimerSlow.Enabled:= False;
+  Button1.Caption:= 'Run';
+  TimerFast.Enabled:=False;
+  LabelInfo.caption:='Idle';
+  Memo1.Lines.Clear;
+  Form1.Caption:='GNS for Prism';
+  Form1.Color:=RGB(100,100,100);
+  SLast:='';
+  LabelInfo.Font.Color:=RGB(255,25,25)
+end;
+
+function TForm1.ObsEnd(SNew:String): Boolean;
+var STemp: String;
+
+begin
+  ObsEnd:=False;
+  if SNew = 'Auto Observations process has been released' then
+    ObsEnd:=True
+end;
+
+function TForm1.StripTimeFromString(var STemp: string):String;
+var
+  Position: Integer;
+begin
+  // Extract string after ':' in line
+  Position := Pos(':', STemp) + 1;
+  Delete(STemp, 1, Position);
+  Result:=STemp;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if TimerSlow.enabled then
+    begin
+      TimerSlow.Enabled:= False;
+      Button1.Caption:= 'Run';
+      TimerFast.Enabled:=False;
+      LabelInfo.caption:='Idle';
+      LabelInfo.Font.Color:=RGB(255,25,25)
+
+    end
+    else
+    begin
+      Memo1.Lines.Clear;
+      Memo1.Lines.Add('Start of sampling at: '+ DateTimeToStr(Now));
+      TimerSlow.Enabled:= True;
+      Button1.Caption:='Stop';
+      TimerFast.Enabled:=True;
+      LabelInfo.Font.Color:=RGB(25,255,25)
+    end;
+end;
+
+
+
+procedure TForm1.CallGNS(var Return: integer; SNew: string);
+// Calls GNS
+var parameters, filename: String;
+
+begin
+  Memo1.Lines.Add(SNew);
+  filename:='C:\Program Files (x86)\GNS\update.vbs';
+  parameters:=' "'+StripTimeFromString(SNew)+'"'+' 120';
+  return:=ShellExecute(handle,'open',Pchar(filename),PChar(parameters),'',SW_MINIMIZE);
+
+  if ObsEnd(SNew) then
+  // Observation ended ok. Send stop instruction to GNS
+  begin
+    filename:='C:\Program Files (x86)\GNS\switchoff.vbs';
+    return:=ShellExecute(handle,'open',Pchar(filename),'','',SW_MINIMIZE);
+    Memo1.Lines.Add('The session has ended. Shutdown is sent to GNS.');
+    Button1Click(self);
+    LabelInfo.caption:='Ready';
+    LabelInfo.Font.Color:=RGB(25,25,255)
+  end;
+end;
+
+procedure TForm1.TimerFastTimer(Sender: TObject);
+// Ticks the clock
+begin
+  LabelInfo.caption:='Running :'+DateTimeToStr(now)
+end;
+
+
+procedure TForm1.TimerSlowTimer(Sender: TObject);
+// Reads the last line of the selected logfile
+var F: TextFile;
+    SNew, STemp: String;
+    return: Integer;
+begin
+   STemp:='';
+   if fileexists(EditFolder.Text) then
+     begin
+
+       AssignFile(F,EditFolder.Text);
+       Reset(F);
+       repeat
+         Readln(F,SNew);
+       until EOF(F);
+       STemp:=Snew;
+       if SNew <> SLast then
+         begin
+          CallGNS(return, SNew);
+          if return < 32 then
+            Memo1.Lines.Add('Problem with talkint to gns. Error: '+ intToStr(Return))
+         end;
+       SLast:=STemp;
+       CloseFile(F)
+     end
+   else
+     Memo1.Lines.Add('File '+EditFolder.Text+' not found!')
+end;
+
+procedure TForm1.W7NavigationButton1Click(Sender: TObject);
+begin
+  if OpenDialog1.execute then
+    EditFolder.Text:= OpenDialog1.FileName;
+end;
+
+end.
